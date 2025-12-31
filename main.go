b5eca7fff49fa24813bc8451cae5e71efc4a018d
q1110 <zeratuliche@gmail.com>
2025-12-31 13:10:21 +0300

    ...

--- main.go
+++ main.go
@@ -17,11 +17,14 @@ import (
 )
 
 func main() {
+	// -- Section: load env variables
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal(err)
 	}
+	// -- end section
 
+	// -- Section: create and setup bot object
 	b, err := tele.NewBot(tele.Settings{
 		Token:     os.Getenv("TOKEN"),
 		Poller:    &tele.LongPoller{Timeout: 10 * time.Second},
@@ -31,13 +34,19 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	// -- end section
 
+	// -- Section: open db connection and setub query executor
 	conn, err := sql.Open("sqlite", "file:mydb.db")
 
 	if err != nil {
 		log.Fatal(err)
 	}
+	ctx := context.Background()
+	q := db.New(conn)
+	// -- end section
 
+	// -- Section: prepare keyboard and buttons
 	menu := &tele.ReplyMarkup{}
 	btnAdd := menu.Data("➕ Дбавить запись", "add")
 	btnDelete := menu.Data("➖ Удалить последнюю запись", "delete")
@@ -53,18 +62,15 @@ func main() {
 		menu.Row(btnGetAllRow),
 		menu.Row(btnDelete),
 	)
-	var message *tele.Message
-	var stateAdd bool
-	ctx := context.Background()
-	q := db.New(conn)
+	// -- end section
 
+	// -- Section: define states
+	var stateAdd bool
+	// -- end section
+
+	// -- Section: define hanlers
 	b.Handle("/menu", func(c tele.Context) error {
-		m, err := b.Send(tele.ChatID(c.Chat().ID), "Привет, я предоставлю тебе все цифры которые тебе нужны!", menu)
-		if err != nil {
-			return c.Send("Ууупс... что-то пошло не так: %v", err)
-		}
-		message = m
-		return nil
+		return c.Send("Привет, я предоставлю тебе все цифры которые тебе нужны!", menu)
 	})
 	b.Handle(&btnAdd, func(c tele.Context) error {
 		stateAdd = true
@@ -72,18 +78,12 @@ func main() {
 	})
 	b.Handle(&btnDelete, func(c tele.Context) error {
 		lastVal, err := q.DeleteLastRow(ctx)
-
 		if err != nil {
 			return c.Send(
 				fmt.Sprintf("Ууупс... что-то пошло не так: %v", err))
 		}
+		return c.Edit(fmt.Sprintf("Запись:\n\n%v: <b>%v</b>\n\nбыла успешно удалена 😉", lastVal.Name, lastVal.Val), menu)
 
-		_, err = b.Edit(message,
-			fmt.Sprintf("Запись:\n\n%v: <b>%v</b>\n\nбыла успешно удалена 😉", lastVal.Name, lastVal.Val), menu)
-		if err != nil {
-			return c.Send("Эта информация уже на экране!")
-		}
-		return nil
 	})
 
 	b.Handle(&btnGetAvatage, func(c tele.Context) error {
@@ -105,11 +105,7 @@ func main() {
 			return c.Send(
 				fmt.Sprintf("Ууупс... что-то пошло не так: %v", err))
 		}
-		_, err = b.Edit(message, fmt.Sprintf("Твое среднее: <b>%.1f</b>\nСреднее какой-то Дашки: <b>%.1f</b>\n\nПо итогу: <b>%.1f</b>", avgAlena.Float64, avgDasha.Float64, avgAlena.Float64-avgDasha.Float64), menu)
-		if err != nil {
-			return c.Send("Эта информация уже на экране!")
-		}
-		return nil
+		return c.Edit(fmt.Sprintf("Твое среднее: <b>%.1f</b>\nСреднее какой-то Дашки: <b>%.1f</b>\n\nПо итогу: <b>%.1f</b>", avgAlena.Float64, avgDasha.Float64, avgAlena.Float64-avgDasha.Float64), menu)
 	})
 
 	b.Handle(&btnGetSalary, func(c tele.Context) error {
@@ -122,15 +118,11 @@ func main() {
 			return c.Send(
 				fmt.Sprintf("Ууупс... что-то пошло не так: %v", err))
 		}
-		_, err = b.Edit(message,
+		return c.Edit(
 			fmt.Sprintf("Твоя ЗП на текущий момент: <b>%v</b>\nА было бы: <b>%v</b>",
 				result.Count*1500+(int64(result.Sum.Float64*0.04)),
 				result.Count*3000,
 			), menu)
-		if err != nil {
-			return c.Send("Эта информация уже на экране!")
-		}
-		return nil
 	})
 
 	b.Handle(&btnGetTotalMonth, func(c tele.Context) error {
@@ -142,11 +134,7 @@ func main() {
 			return c.Send(
 				fmt.Sprintf("Ууупс... что-то пошло не так: %v", err))
 		}
-		_, err = b.Edit(message, fmt.Sprintf("Всего в этом месяце: <b>%.1f</b>", r.Float64), menu)
-		if err != nil {
-			return c.Send("Эта информация уже на экране!")
-		}
-		return nil
+		return c.Edit(fmt.Sprintf("Всего в этом месяце: <b>%.1f</b>", r.Float64), menu)
 	})
 	b.Handle(&btnGetAllRow, func(c tele.Context) error {
 		r, err := q.GetAllRowsInMonth(ctx, db.GetAllRowsInMonthParams{
@@ -161,11 +149,7 @@ func main() {
 		for _, v := range r {
 			fmt.Fprintf(&msg, "%v.%v -- %v: <b>%v</b>\n", v.Month, v.Year, v.Name, v.Val)
 		}
-		_, err = b.Edit(message, msg.String(), menu)
-		if err != nil {
-			return c.Send("Эта информация уже на экране!")
-		}
-		return nil
+		return c.Edit(msg.String(), menu)
 	})
 
 	b.Handle(tele.OnText, func(c tele.Context) error {
@@ -174,44 +158,36 @@ func main() {
 			if len(vals) != 2 {
 				return c.Send("Необходимо ввести все значение в формате имя|начение")
 			}
-
 			nameLower := strings.ToLower(vals[0])
 			if nameLower != "даша" && nameLower != "алена" {
 				return c.Send(
 					"Допустимые имена:\n\nДаша\nАлена\n\nможешь писать их с маленькой или большой буквы - это не важно, но другие имена не допустимы!")
 			}
-
 			intValue, err := strconv.ParseInt(vals[1], 10, 64)
 			if err != nil {
 				return c.Send(
 					fmt.Sprintf("%v -- второе значение после /add должно быть числом", vals[1]))
 			}
-
 			err = q.CreateRow(ctx, db.CreateRowParams{
 				Name:  nameLower,
 				Val:   intValue,
 				Month: int64(time.Now().Month()),
 				Year:  int64(time.Now().Year()),
 			})
-
-			if err != nil {
-				return c.Send(
-					fmt.Sprintf("Ууупс... что-то пошло не так: %v", err))
-
-			}
-			m, err := b.Send(tele.ChatID(c.Chat().ID), "Запись была успешно добавлена 😉", menu)
 			if err != nil {
 				return c.Send(
 					fmt.Sprintf("Ууупс... что-то пошло не так: %v", err))
 
 			}
 			stateAdd = false
-			message = m
-			return nil
+			return c.Send("Запись была успешно добавлена 😉", menu)
 		}
 		return nil
 	})
+	// -- end section
 
+	// -- Section: start app
 	b.Start()
+	// -- end section
 
 }
